898e9b558b0dfad6dce8dde44609df48446af71a
unknown <onuryucel200@hotmail.com>
2026-04-16 21:35:24 +0300

    v0.17| kod dosyalarinda hata var... bazi noktalarda README.md ile uyumsuzluk bulunmakta

--- 04-Cizgi Algoritmalari/kod/src/main.cpp
+++ 04-Cizgi Algoritmalari/kod/src/main.cpp
@@ -59,6 +59,43 @@ void drawPixel(int x, int y, Color_t color)
     colorBuffer[y * WindowWidth + x] = color;
 }
 
+void ddaLineAlgo(int x0, int y0, int x1, int y1, Color_t color)
+{
+    int deltaX = x1 - x0;
+    int deltaY = y1 - y0;
+
+    /*
+        if |deltaX| >= |deltaY|
+            sideLength = |deltaX|
+
+        else
+            sideLength = |deltaY|
+    */
+    int sideLength = abs(deltaX) >= abs(deltaY) ? abs(deltaX) : abs(deltaY);
+
+    float incX = deltaX / (float)sideLength;
+    float incY = deltaY / (float)sideLength;
+
+    float currentX = x0;
+    float currentY = y0;
+
+    for (size_t i = 0; i <= sideLength; i++)
+    {
+        drawPixel(round(currentX), round(currentY), color);
+        currentX += incX;
+        currentY += incY;
+    }
+}
+
+void drawLine(int x0, int y0, int x1, int y1, Color_t color)
+{
+    ddaLineAlgo(x0, y0, x1, y1, color);
+}
+
+//-------------------------------------------------------------------------//
+//-------------------------------------------------------------------------//
+//-------------------------------------------------------------------------//
+
 void initSDL()
 {
     window = SDL_CreateWindow("TrabzonCaydanligi", WindowWidth, WindowHeight, NULL);

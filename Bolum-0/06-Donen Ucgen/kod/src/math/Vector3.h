7638dde3c1fbebb5e65f1603aa95e59e29fe9777
unknown <onuryucel200@hotmail.com>
2026-08-09 09:37:55 +0300

    v0.92| 06-| kodu duzeltildi

--- Bolum-0/06-Donen Ucgen/kod/src/math/Vector3.h
+++ Bolum-0/06-Donen Ucgen/kod/src/math/Vector3.h
@@ -2,6 +2,10 @@
 
 #include <cmath>
 
+#include "Vector4.h"
+
+class Vector4;
+
 class Vector3
 {
 public:
@@ -9,16 +13,25 @@ public:
 	Vector3(float x_, float y_, float z_);
 	~Vector3();
 
+	Vector4 toVec4();
+
 	[[nodiscard]] Vector3 operator+ (const Vector3& vec);
 	[[nodiscard]] Vector3 operator- (const Vector3& vec);
 	[[nodiscard]] Vector3 operator* (float s);
 	[[nodiscard]] Vector3 operator/ (float s);
 
 	void normalize();
+
+	Vector3 rotateX(float alfa);
+	Vector3 rotateY(float alfa);
+	Vector3 rotateZ(float alfa);
+
+	
 	float dot(const Vector3& vec);
 	Vector3 cross(const Vector3& vec);
 
-	float length();
+	static float length(Vector3& vec);
+
 
 	float x, y, z;
 
